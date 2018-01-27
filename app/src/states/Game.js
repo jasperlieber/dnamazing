897797a028b6fd1d11d230e29827377f2184eec6
matt.brelsford <mbrelsford@hitpointstudios.com>
2018-01-27 17:53:42 -0500

    soft mate

--- app/src/states/Game.js
+++ app/src/states/Game.js
@@ -1,33 +1,188 @@
 /* globals __DEV__ */
 import Phaser from 'phaser'
+import $ from 'jquery'
+import _ from 'underscore'
 
-
+import HudView from './HudView'
 
 import GenomeData from "../../assets/data/genome"
 
-
 export default class extends Phaser.State {
-  init () {}
+  init () {
+
+  }
   preload () {}
   create () {
 
+    let that = this;
+
     this.colors=[0xcc3333, 0x999933, 0x3333cc, 0x33cc33]
-    this.drawTree(GenomeData, game.width, game.height, 0, 0, 0);
+
+    this.hudView = new HudView({
+        "model": null,
+        "state": this
+    });
+    this.$htmlWrapper = $("#html-wrapper");
+    this.$htmlWrapper.append(this.hudView.$el);
+    // localStorage.clear();
+
+    this.myId = null;
+
+    let myIdString = localStorage.getItem("my_id");
+    if(myIdString){
+      this.myId = parseInt(myIdString);
+    }
+
+    
+    this.myTreeGroup = game.add.group();
+    this.myGenome = null;
+
+
+    if(this.myId){
+      // TODO: Get Data from Jasper
+
+      $.ajax({
+        "url": "http://35.227.37.79/dna.php?getuser&id="+this.myId,
+        "method": "GET",
+        "success": function( response ) {
+            that.myTreeGroup = game.add.group();
+            that.myGenome = JSON.parse(response.genome);
+
+            that.drawTree(that.myGenome, game.width, game.height, 0, 0, 0, that.myTreeGroup);
+        }
+      })
+
+
+    }else{
+
+      // TODO: Tell Jasper to create me
+      let myInitialColor = game.rnd.integerInRange(0, this.colors.length-1);
+
+      let myInititalGenomeString = "{\"color\":" + myInitialColor + "}";
+      $.ajax({
+        "url": "http://35.227.37.79/dna.php?newuser&genome=" + encodeURIComponent(myInititalGenomeString),
+        // "url": "http://35.227.37.79/api.php",
+        "method": "GET",
+        "success": function( response ) {
+            let parsed = JSON.parse(response);
+            that.myId = parsed.id;
+
+            localStorage.setItem("my_id", this.myId)
+            that.myTreeGroup = game.add.group();
+
+            that.myGenome = JSON.parse(myInititalGenomeString);
+            
+            that.drawTree(that.myGenome, game.width, game.height, 0, 0, 0, that.myTreeGroup);
+        }
+      })
+
+    }
+
+
+
+    this.allUsers = [];
+
+
+    $.ajax({
+      "url": "http://35.227.37.79/dna.php?getallusers",
+      "method": "GET",
+      "success": function(response){
+        
+        let parsed = JSON.parse(response);
+
+        for(let i = 0;i<parsed.allUsers.length;i++){
+          parsed.allUsers[i].genome = JSON.parse(parsed.allUsers[i].genome)
+        }
+        that.allUsers = parsed.allUsers;
+        that.createUserButtons(parsed.allUsers);
+
+      }
+    })
+
 
   }
   render () {}
 
 
-  drawTree(node, w, h, x, y, depth){
+  drawTree(node, w, h, x, y, depth, startGroup){
 
     if(typeof(node.color) !== "undefined"){
+
       let sq = game.add.sprite(x,y,"square");
       sq.tint = this.colors[node.color]
       sq.width = w;
       sq.height = h;
-    }else{
-      this.drawTree(node["0"], w, h/2, x, y, depth+1);
-      this.drawTree(node["1"], w, h/2, x, y + (h/2), depth+1);
+      startGroup.add(sq);
+
+    }else if( typeof(node["0"]) !== "undefined" && typeof(node["1"]) !== "undefined" ){
+
+      if(depth%2===0){
+        this.drawTree(node["0"], w, h/2, x, y, depth+1, startGroup);
+        this.drawTree(node["1"], w, h/2, x, y + (h/2), depth+1, startGroup);
+      }else{
+        this.drawTree(node["0"], w/2, h, x, y, depth+1, startGroup);
+        this.drawTree(node["1"], w/2, h, x + (w/2), y, depth+1, startGroup);
+      }
+
     }
+
   }
+
+  createUserButtons(userList){
+
+    for(let i = 0;i<userList.length;i++){
+      let g = game.add.group();
+      this.drawTree(userList[i].genome, 512, 512, 0, 0, 0, g);
+      let tex = g.generateTexture();
+      userList[i].dataUrl = tex.getCanvas().toDataURL();
+      g.destroy();
+    }
+
+    this.hudView.renderUserButtons(userList);
+
+
+  }
+
+
+  doMate(otherUserId){
+
+
+
+    for(let i = 0;i<length;i++){
+      $.ajax({
+        "onComplete": function(){
+          console.log(i)
+        }
+      })
+    }
+
+
+    console.log(otherUserId);
+    let otherUserData =  _.findWhere(this.allUsers, {id: "" + otherUserId + ""});
+    console.log("OTHER USER DATA", otherUserData);
+
+
+    if(otherUserData){
+
+      let newTree = {
+        "0": this.myGenome,
+        "1": otherUserData.genome
+      };
+      console.log("Other User To Mate With: ", otherUserData);
+
+      this.myGenome = newTree;
+      this.myTreeGroup.removeAll();
+
+      this.drawTree(this.myGenome, game.width, game.height, 0, 0, 0, this.myTreeGroup);
+
+
+    }
+
+
+  }
+
+
+
+
+
 }
---
+++ app/src/states/HudView.js
@@ -0,0 +1,63 @@
+import { _ } from 'underscore';
+import $ from 'jquery';
+import { View } from 'backbone';
+
+const mainTemplate = require('html-loader?root=.!./HudTemplate.html')
+
+// class DiscoveryView extends View {
+export default class extends View {
+	constructor ({ el, model, state }){
+		super({
+			el: el, 
+			model: model,
+			className: "river-tubing-wrapper",
+			events: {
+				"click #user-buttons button": "doMate"
+			}
+		})
+
+		this.state = state;
+		this.template = _.template(mainTemplate);
+
+    	this.render();
+	}
+	initialize(){
+		// this.listenTo(this.model, "change", this.render);
+	}
+	render(){
+		
+		var html = this.template({
+			"model": this.model
+		});
+		this.$el.html(html);
+
+		this.$userButtons = this.$el.find("#user-buttons")
+		// this.$promptText = this.$el.find(".prompt-text");
+    	
+	}
+
+
+	renderUserButtons(userList){
+		this.$userButtons.empty();
+		for(let i = 0;i<userList.length;i++){
+			let user = userList[i];
+			let newButton = $("<button style=\"background-image: url(" + user.dataUrl + ");\" data-user-id=\"" + user.id + "\" ></button>");
+			this.$userButtons.append(newButton);
+		}
+
+
+
+	}
+	doMate(e){
+		e.preventDefault();
+
+		let $currentTarget = $(e.currentTarget);
+		let otherid = $currentTarget.data("user-id");
+		this.state.doMate(otherid);
+
+		return false;
+	}
+}
+
+
+
